ccd1db88cb8e811b2418b62cf6c5ce002b22f8a1
Oguz Kagan BATI <oguzkaganbati@gmail.com>
2023-12-06 16:36:06 +0300

    Global Exception Handler

--- src/main/java/com/tobeto/rentACar/core/utilities/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/tobeto/rentACar/core/utilities/exceptions/GlobalExceptionHandler.java
@@ -1,12 +1,13 @@
 package com.tobeto.rentACar.core.utilities.exceptions;
 
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,28 +17,25 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler({MethodArgumentNotValidException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<Object> handleValidationError(MethodArgumentNotValidException exception) {
+    public ResponseEntity<Map<String, String>> handleValidationError(MethodArgumentNotValidException exception) {
         Map<String, String> errors = new HashMap<>();
-        exception.getBindingResult().getFieldErrors().forEach((error) -> {
-            String fieldName = error.getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
+        exception.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
         });
-
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
-    }
-
-    @ExceptionHandler({RuntimeException.class})
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ResponseEntity<Object> handleRuntimeExceptionError(RuntimeException exception) {
-        String errorMessage = "An unexpected error occurred-->> " + exception.getMessage();
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+        return ResponseEntity.badRequest().body(errors);
     }
 
     @ExceptionHandler({Exception.class})
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ResponseEntity<Object> handleError(Exception exception) {
-        String errorMessage = "An unexpected error occurred-->> " + exception.getMessage() + exception.getCause();
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+    public String handleError(Exception exception) {
+        return "An unexpected error occurred-->> " + exception.getMessage() + exception.getCause();
     }
+
+    @ExceptionHandler({RuntimeException.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleRuntimeExceptionError(RuntimeException exception) {
+        return "An unexpected error occurred-->> " + exception.getMessage();
+    }
+
+
 }
